96491109e6d9e01a3e537c9f6463991864969e88
Илья Северин <ilya.severin2018@gmail.com>
2021-05-27 21:02:36 +0300

    Добавьте файлы проекта.

---
+++ Dinner/Philosopher.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dinner
+{
+    class Philosopher
+    {
+        private int number;
+        public int eaten;
+
+        public Philosopher(int number)
+        {
+            this.number = number;
+            eaten = 0;
+        }
+
+       private void GetFork(List<Fork> forks)
+       {
+            
+                int firstFork = number;
+                int secondFork;
+                if (number + 1 == forks.Count())
+                {
+                    secondFork = 0;
+                }
+                else
+                {
+                    secondFork = number + 1;
+                }
+                if (!forks[firstFork].IsBusy && !forks[secondFork].IsBusy)
+                {
+                    forks[firstFork].IsBusy = true;
+
+                    Thread.Sleep(1);
+                    forks[secondFork].IsBusy = true;
+                    
+                    forks[firstFork].IsBusy = false;
+                    Thread.Sleep(1);
+                    forks[secondFork].IsBusy = false;
+                    eaten++;
+
+                }
+            
+               
+       }
+        public void StartWork(List<Fork> forks)
+        {
+            while(true)
+            {
+                Think();
+                Eat(forks);
+            }
+
+        }
+        private void Eat(List<Fork> forks)
+        {
+            if (forks.Where(f => f.IsBusy).Count() < forks.Count())
+            {
+                GetFork(forks);
+                Thread.Sleep(1);
+            }
+        }
+                
+            
+        
+
+        private void Think()
+        {
+            Thread.Sleep(1);
+        }
+    }
+}
---
+++ Dinner/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dinner
+{
+    class Program
+    {
+        static List<Fork> forks = new List<Fork>();
+        static List<Philosopher> philosophers = new List<Philosopher>();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                forks.Add(new Fork());
+                philosophers.Add(new Philosopher(i));
+            }
+            Thread thread1 = new Thread(()=>philosophers[0].StartWork(forks));
+            Thread thread2 = new Thread(() => philosophers[1].StartWork(forks));
+            Thread thread3 = new Thread(() => philosophers[2].StartWork(forks));
+            Thread thread4 = new Thread(() => philosophers[3].StartWork(forks));
+            Thread thread5 = new Thread(() => philosophers[4].StartWork(forks));
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+            thread5.Start();
+
+            while(true)
+            {
+                Thread.Sleep(5000);
+                double sum = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    sum += philosophers[i].eaten;
+                }
+
+                Console.WriteLine(philosophers[0].eaten.ToString() + "|" + philosophers[1].eaten.ToString() + "|" + philosophers[2].eaten.ToString() + "|" + philosophers[3].eaten.ToString() + "|" + philosophers[4].eaten.ToString());
+                Console.WriteLine(philosophers[0].eaten / sum * 100 + "|" + philosophers[1].eaten / sum * 100 + "|" + philosophers[2].eaten / sum * 100 + "|" + philosophers[3].eaten / sum * 100 + "|" + philosophers[4].eaten / sum * 100);
+                Console.WriteLine("-------------------------------------------------");
+            }
+            
+        }
+    }
+
+
+}
